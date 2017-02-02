b6dfef86808a953ea1dba35bd9c1024621001b49
zulkiflireza <zulkiflireza10@gmail.com>
2017-02-02 22:18:19 +0700

    Buat Luaslingkaran

--- HitungLuas2.java
+++ HitungLuas2.java
@@ -4,6 +4,10 @@ class HitungLuas2 {
 		return panjang * lebar;
 	}
 
+public static float cariLuasLingkaran(int jarijari) {
+		return (22 * jarijari * jarijari) / 7;
+	}
+
 	public static void main (String[] args) {
 
 	}
