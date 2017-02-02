1310cf42331bcb796ac5725b3ff0edde40b43e55
zulkiflireza <zulkiflireza10@gmail.com>
2017-02-02 22:22:15 +0700

    Buat LuasSegitiga

--- HitungLuas2.java
+++ HitungLuas2.java
@@ -3,6 +3,9 @@ class HitungLuas2 {
 	public static int cariLuasPersegiPanjang(int panjang, int lebar) {
 		return panjang * lebar;
 	}
+public static int cariLuasSegitiga(int alas, int tinggi) {
+		return (alas * tinggi) / 2;
+	}
 
 	public static void main (String[] args) {
 
